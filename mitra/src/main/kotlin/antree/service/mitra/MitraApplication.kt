fbb5bb6dcbc79ce31fcf3d664fe681d8f6a56f63
agungdea <deaagung@gmail.com>
2020-10-01 22:01:09 +0700

    mitra init

---
+++ mitra/src/main/kotlin/antree/service/mitra/MitraApplication.kt
@@ -0,0 +1,11 @@
+package antree.service.mitra
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MitraApplication
+
+fun main(args: Array<String>) {
+	runApplication<MitraApplication>(*args)
+}
---
+++ mitra/src/test/kotlin/antree/service/mitra/MitraApplicationTests.kt
@@ -0,0 +1,13 @@
+package antree.service.mitra
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MitraApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
